3f1a678fa6825519337d70641fc4ccbd617a4a54
UM YEJI <20247141@edu.hanbat.ac.kr>
2026-05-23 05:14:24 +0000

    반복

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // 콘솔 창에 문정을 출력합니다.
+        // 콘솔 창에 문장을 출력합니다.
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
     }
 }
